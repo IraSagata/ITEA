a68318faa0a1587b6223e8669f39cf906b5ca814
IraSagata <irasagata@gmail.com>
2019-08-15 21:33:01 +0300

    add changes

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("hi");
+// function checkNumber(num) {
+//   if (num < 50 && num <= 20) {
+//     console.log("true");
+//   } else {
+//     console.log("fauls");
+//   }
+// }
+// checkNumber(20);
+// const fn = (a) => a > 50 && a <= 20 ? true: faulf
+// function checkNumber(a, b) {
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+//     console.log(a, b);
+//   }
+//   checkNumber(2, 3);
