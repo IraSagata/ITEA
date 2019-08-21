97856e63a70d666198d2b4136db7cfefa6596363
IraSagata <irasagata@gmail.com>
2019-08-21 12:42:20 +0300

    homework2 initial file

--- HomeWork-1/script.js
+++ HomeWork-1/script.js
@@ -10,3 +10,23 @@ user.__proto__ = userCharacteristics;
 console.log(user.name);
 console.log(user.bornDate);
 console.log(user.age);
+
+// function Person (name, surname){
+//   this.name = name;
+//   this.surname = surname;
+//   this.sayHello = function() {
+//   console.log(`hello  ${name}  ${surname}`)
+//   }
+//   }
+//   const person = new Person( 'Iryna', 'Sagata');
+//   person.sayHello()
+
+// function Calculator (){
+
+//   this.sayHello = function(firstNumber, secondNumber) {
+//     let sum = firstNumber * secondNumber;
+//   console.log(sum)
+//   }
+//   }
+// const calc = new Calculator()
+//  calc.sayHello(2, 5)
---
+++ HomeWork-2/script.js
@@ -0,0 +1,35 @@
+//  Створіть функцію-конструктор яка буде мати настпні методи:
+//   sum(); додавання
+//   multiply() множення
+//   divide() ділення
+//   subtraction() віднімання
+//  Також у функції-конструктор повина бути локальна зміна типу const result = “The result is:”
+//   і ви маєте її використовувати у методах вказаних вище, наприклад:
+//   const calc = new Calculator();
+//   calc.sum(2,3) // виведе  “The result is: 5”;
+//   calc.divide(9,3) // виведе  “The result is: 3”;
+//    і тд….
+// HOMEWORK 2
+function Calculator() {
+  this.multiply = function(firstNumber, secondNumber) {
+    let multiplyTwoNumbers = firstNumber * secondNumber;
+    console.log(`The result is: ${multiplyTwoNumbers}`);
+  };
+  this.sum = function(firstNumber, secondNumber) {
+    let summariseTwoNumbers = firstNumber + secondNumber;
+    console.log(`The result is:  ${summariseTwoNumbers}`);
+  };
+  this.divide = function(firstNumber, secondNumber) {
+    let divideTwoNumbers = firstNumber / secondNumber;
+    console.log(`The result is: ${divideTwoNumbers}`);
+  };
+  this.sub = function(firstNumber, secondNumber) {
+    let subtractionTwoNumbers = firstNumber + secondNumber;
+    console.log(`The result is: ${subtractionTwoNumbers}`);
+  };
+}
+const calc = new Calculator();
+calc.sum(2, 5);
+calc.divide(9, 3);
+calc.multiply(9, 3);
+calc.sub(9, 3);
