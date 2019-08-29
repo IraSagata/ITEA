4a473e7850726686f146165ddf4ada06dbf34a99
IraSagata <irasagata@gmail.com>
2019-08-29 19:20:08 +0300

    q

--- HomeWork-4/script.js
+++ HomeWork-4/script.js
@@ -14,23 +14,44 @@
 // });
 // academy.addStudent = "Yriy Petrichenko";
 // console.log(academy.students);
+
+// VARTION 2
+// console.clear();
+// const academy = {
+//   students: []
+// };
+
+// Object.defineProperty(academy, "addStudent", {
+//   set: function(value) {
+//     let arr = value.split(" ");
+//     let student = {
+//       studentName: arr.length > 0 ? arr[0] : "none",
+//       studentSurname: arr.length > 1 ? arr[1] : "none",
+//       createDate: Date()
+//     };
+
+//     this.students.push(student);
+//   }
+// });
+
+// academy.addStudent = "Yriy Petrichenko";
+// console.log(academy.students);
+
 console.clear();
-academy = {
+const academy = {
   students: []
 };
 
 Object.defineProperty(academy, "addStudent", {
   set: function(value) {
-    let arr = value.split(" ");
-    let student = {
-      studentName: arr.length > 0 ? arr[0] : "none",
-      studentSurname: arr.length > 1 ? arr[1] : "none",
-      createDate: Date()
-    };
-
-    this.students.push(student);
+    let newStudent = value.split(" ");
+    this.students.push({
+      createDate: Date(),
+      studentName: value[0],
+      studentSurname: value[1]
+    });
+    return academy.students;
   }
 });
-
 academy.addStudent = "Yriy Petrichenko";
 console.log(academy.students);
