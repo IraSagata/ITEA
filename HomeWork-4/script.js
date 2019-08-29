374ad74c86c80e0789a204e50347c55cb1a86afb
IraSagata <irasagata@gmail.com>
2019-08-29 19:00:49 +0300

    version2

--- HomeWork-4/script.js
+++ HomeWork-4/script.js
@@ -1,16 +1,36 @@
+// console.clear();
+// const academy = {
+//   students: []
+// };
+
+// Object.defineProperty(academy, "addStudent", {
+//   set: function(value) {
+//     this.students.createDate = Date().toString();
+//     [this.students.studentName, this.students.studentSurname] = value.split(
+//       " "
+//     );
+//     return academy.students;
+//   }
+// });
+// academy.addStudent = "Yriy Petrichenko";
+// console.log(academy.students);
 console.clear();
-const academy = {
+academy = {
   students: []
 };
 
 Object.defineProperty(academy, "addStudent", {
   set: function(value) {
-    this.students.createDate = Date().toString();
-    [this.students.studentName, this.students.studentSurname] = value.split(
-      " "
-    );
-    return academy.students;
+    let arr = value.split(" ");
+    let student = {
+      studentName: arr.length > 0 ? arr[0] : "none",
+      studentSurname: arr.length > 1 ? arr[1] : "none",
+      createDate: Date()
+    };
+
+    this.students.push(student);
   }
 });
+
 academy.addStudent = "Yriy Petrichenko";
 console.log(academy.students);
