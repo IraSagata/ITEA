46bc94f7f3cf210b1d7bf2d3e045ab138d8ece29
IraSagata <irasagata@gmail.com>
2019-09-10 17:13:39 +0300

    just started

---
+++ ticTacToeGame-gh-pages/js/game.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 9; i++) {
+  document.getElementById("game").innerHTML +=
+    "<button class='block'></button>";
+}
+const game = document.getElementById("game");
+buttons.addEventListener("click", () => {
+  let step = 0;
+  if (event.target.className == "block") {
+    if (step == 0) {
+      event.target.innerHTML = "x";
+    } else {
+      event.target.innerHTML = "0";
+    }
+    step++;
+  }
+});
