450bd4dd0a72175601ed80b06d49e969e5917497
IraSagata <irasagata@gmail.com>
2019-09-10 16:42:32 +0300

    some changes

--- HomeWork-5/script.js
+++ HomeWork-5/script.js
@@ -1,19 +1,20 @@
 const inp = document.querySelector("input");
 const addTaskBtn = document.querySelector("button");
 const list = document.querySelector("ul");
-const checkBox = document.getElementsByClassName("check");
+const checkBox = document.getElementsByClassName("check")[0];
 
 function getInputValue(inputElement) {
-  console.log("getInputValue ", inputElement);
+  //console.log("getInputValue ", inputElement);
   return inputElement.value;
 }
 
 function createElementWithText(elementName, text) {
-  console.log("createElementWithText ", elementName, text);
+  //console.log("createElementWithText ", elementName, text);
   let span = document.createElement("span");
   span.textContent = "\u00D7";
   span.onclick = function() {
     let li = this.parentElement;
+    console.log("li");
     list.removeChild(li);
   };
   let li = document.createElement(elementName);
@@ -23,19 +24,41 @@ function createElementWithText(elementName, text) {
 }
 
 function appendChildTodo(parent, child) {
-  console.log("appendChildTodo ", parent, child);
+  //console.log("appendChildTodo ", parent, child);
   parent.appendChild(child);
-  return (inp.value = "");
+  // return (inp.value = "");
 }
 
 addTaskBtn.addEventListener("click", () => {
   let value = getInputValue(inp);
-
   if (value) {
-    console.log("click ", value);
-
     let child = createElementWithText("li", value);
-
-    return appendChildTodo(list, child);
+    if (checkBox.checked) {
+      child.style.fontWeight = "bold";
+      child.style.color = "blue";
+      list.insertBefore(child, list.firstChild);
+    } else {
+      appendChildTodo(list, child);
+    }
+    return (inp.value = "");
+  } else {
+    alert("Come on!!!");
   }
 });
+inp.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    addTaskBtn.click();
+  }
+});
+
+// list.addEventListener(
+//   "click",
+//   function(event) {
+//     console.log("click");
+//     if ("li") {
+//       inp.style.textDecoration = "lineThrough";
+//       list.replaceChild();
+//     }
+//   },
+//   false
+// );
